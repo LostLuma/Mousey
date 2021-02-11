dac0be28cf101a04d44828cc652a62534d8ae491
SnowyLuma <lilly@snowyluma.dev>
2021-02-11 19:59:02 +0100

    dashboard: Remove BigInt literals
    These cause the build to fail sadly.

--- packages/dashboard/src/Archive/utils.js
+++ packages/dashboard/src/Archive/utils.js
@@ -1,8 +1,8 @@
-const DISCORD_EPOCH = 1420070400000n;
+const DISCORD_EPOCH = BigInt("1420070400000");
 
 export function snowflakeTime(value) {
   const snowflake = BigInt(value);
-  const timestamp = (snowflake >> 22n) + DISCORD_EPOCH;
+  const timestamp = (snowflake >> BigInt("22")) + DISCORD_EPOCH;
 
   return new Date(Number(timestamp));
 }
