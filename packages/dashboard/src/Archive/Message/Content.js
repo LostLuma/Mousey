f5b27fbce52ee83ebf58c1176d3e3d623cf45475
LostLuma <lilly@lostluma.dev>
2021-05-31 12:03:48 +0200

    dashboard: Remvoe simple-markdown dependency for now
    I'll add markdown rendering in the future when I have time to.

--- packages/dashboard/src/Archive/Message/Content.js
+++ packages/dashboard/src/Archive/Message/Content.js
@@ -1,11 +1,9 @@
 import React from "react";
-import SimpleMarkdown from "simple-markdown";
+// import SimpleMarkdown from "simple-markdown";
 
 import "./Content.css";
 
 export default function Content({ data }) {
-  const { content, edited_at: editedAt, mentions } = data;
-
-  // TODO: Render markdown etc.
-  return <div className="content">{content}</div>;
+  // const { content, edited_at: editedAt, mentions } = data;
+  return <div className="content">{data.content}</div>;
 }
